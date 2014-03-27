53a04dd9a7dfa066069a0f26d8a0f5503d24e8ce
Sindre Sorhus <sindresorhus@gmail.com>
2014-03-27 18:11:52 +0100

    init

---
+++ test.js
@@ -0,0 +1,9 @@
+'use strict';
+var assert = require('assert');
+var xmlChars = require('./index');
+
+it('should match valid XML Character Classes characters', function () {
+	assert(xmlChars.letter.test('unicorn'));
+	assert(xmlChars.digit.test('0'));
+	assert(!xmlChars.letter.test('#'));
+});
